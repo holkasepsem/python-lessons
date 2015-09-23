a9217e058658956993aa1784f0578be57159a669
E <evickasmahelova@Evickas-MacBook-Air.local>
2015-09-23 21:57:09 +0200

    Add a new file

---
+++ guess_number.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Toto je Python: `Guess a number!`
+
+TODO: Přidáme počítadlo pokusů!
+FIXME: Ošetři `ValuError` při zadání nesprávné hodnoty.
+"""
+
+import random
+
+
+def guess_number():
+    """
+    Funkce nebo také procedura.
+    """
+
+    guess_number = 0
+    number = random.randint(1, 20)
+
+    print("Ahoj, myslím si číslo od jedné do dvaceti.")
+    print("Zkus si tipnout, které číslo si myslím.")
+    
+    while number != guess_number:
+        try:
+            guess_number = int(input("Zadej číslo: "))
+        except ValueError:
+            print("Nezadal jsi číslo, zkus to znovu!")
+            continue
+
+        if number == guess_number:
+            print("Hurááá, uhodl jsi!")
+            break
+
+        elif number > guess_number:
+            print("Myslel jsem si větší číslo!")
+
+        else:
+            print("Myslel jsem si menší číslo!")
+
+if __name__ == "__main__":
+    guess_number()
