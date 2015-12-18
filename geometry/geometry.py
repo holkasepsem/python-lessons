28a7c9b34cd88c0cdafaa83f004a134a8c8085bb
Landa David <david.landa@rozhlas.cz>
2015-12-18 19:19:47 +0100

    Create a generic potint class

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -2,44 +2,65 @@
 Implementace základních geometrických objektů.
 """
 
+import abc
 
-class Point():
-	def __init__(self, x=0, y=0, z=0):
-		self.x = x
-		self.y = y
-		self.z = z
 
+# -----------------------------------------------------------------------------
+class Point:
+
+
+    """
+    Třída reprezentující bod v n-rozměrném euklidovském prostoru
+    (prozatím jen kartézské souřadnice).
+    """
+
+    def __init__(self, *coordinates):
+        """
+        Vytvoří bod s požadovanými souřadnicemi.
+        :coordinates: Kartézké souřadnice.
+        """
+        try:
+            self._coordinates = tuple((float(value) for value in coordinates))
+            if not len((self._coordinates)):
+                raise ValueError("Musíte zadat alespoň jednu souřadnici!")
+        except: 
+            raise ValueError("Souřadnice musí být sekvence čísel!")
+        
+        self._dimension = len(self.coordinates)
+
+        for index in range(len(coordinates)):
+            setattr(Point, "x" + str(index + 1), 
+                property(lambda self, 
+                    index=index: str(self.coordinates[index])))
+            
+    @property # getter
+    def coordinates(self):
+        return self._coordinates
+
+    @coordinates.setter
+    def coordinates(self, value):
+        # TODO Ošetři podobně jako v `__init__`!
+        self._coordinates = value
+        self._dimension = len(self.coordinates)
+        
+    @property
+    def dimension(self):
+        return self._dimension
+
+    
+# -----------------------------------------------------------------------------
 class Line():
-	def __init__(self, p1, p2):
-		self.p1 = p1
-		self.p2	= p2
+    def __init__():
+        pass
 
-
+# =============================================================================
+# SIMPLE TESTS
+# =============================================================================
 if __name__ == "__main__":
 
-	# Vytvářím objekt reprezentující bod s výchozími hodnotami
-	p1 = Point()
-	print(p1.x, p1.y, p1.z)
-
-	p2 = Point(0, 0, 0)
-	print(p2.x, p2.y, p2.z)
-	
-	p3 = Point(0, 0, 1)
-	print(p3.x, p3.y, p3.z)
-	
-	p4 = Point(z=1)
-	print(p4.x, p4.y, p4.z)
-	
-	p5 = Point(z=3, y=2, x=1)
-	print(p5.x, p5.y, p5.z)
-	
-	p6 = Point(1, 2, 3)
-	print(p6.x, p6.y, p6.z)
-
-
-	l1 = Line(p1=Point(), p2=Point(1,1))
-
-	print(l1.p1, l1.p2)
-	print(l1.p1.x, l1.p2.y)
-
-
+    p = Point(1, 2, 3, 4, 8.0, 9.0)
+    print(p.coordinates)
+    print(p.dimension)
+    print(p.x1, p.x2, p.x3, p.x4, p.x5, p.x6)
+    print(type(p.x1))
+    print(type(p.coordinates)) 
